2f37b5a9f037856145dfc053ef63d3daf073d651
ilya-grafskiy <il.grafskiy@gmail.com>
2022-03-20 23:16:13 +0300

    Добавьте файлы проекта.

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace WebDz1.Controllers
+{
+    public class HelloController : Controller
+    {
+        // 
+        // Вариант с простым вызовом файла Index из папки View
+
+        /*
+        public IActionResult Index()
+        {
+            return View();
+        }
+        */
+
+
+        public IActionResult Index(string name, int age)
+        {
+                        
+            ViewData["N"] = name;
+            ViewData["a"] = age;
+            ViewData["info"] = "Ваше имя: " + name + "возраст ваш " + age;
+
+
+            return View();
+        }
+
+
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        // Requires using System.Text.Encodings.Web;
+
+        /*
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+        */
+
+        // https://localhost:7285/Hello/Welcome/3?name=Rick 
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
---
+++ Controllers/Lesson9Controller.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebDz1.Controllers
+{
+    public class Lesson9Controller : Controller
+    {
+        public IActionResult Index(string textik = "test")
+        {
+            string itog;
+
+            ViewData["textik"] = textik;
+
+            
+            char[] obrtextik = textik.ToCharArray();
+            Array.Reverse(obrtextik);
+            string finaltextik = new string(obrtextik);
+
+            if (textik == finaltextik)
+            {
+                ViewData["itog"] = ("Данная строка палидром");
+            }
+            else
+            {
+                ViewData["itog"] = ("Данная строка не является палидромом");
+            }
+           
+
+            return View();
+        }
+    }
+}
