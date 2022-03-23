6630887d0b0b977b445efd0faf228600c7494e21
Ilya Grafskiy <zzz@zzz.zz>
2022-03-23 09:40:27 +0300

    Добавил Index2 для WinForm, в файле Index поправил Action для многократной проверки

--- Controllers/Lesson9Controller.cs
+++ Controllers/Lesson9Controller.cs
@@ -17,15 +17,37 @@ namespace WebDz1.Controllers
 
             if (textik == finaltextik)
             {
-                ViewData["itog"] = ("Данная строка является палидромом");
+                ViewData["itog"] = ("Данная строка является палиндромом");
             }
             else
             {
-                ViewData["itog"] = ("Данная строка не является палидромом");
+                ViewData["itog"] = ("Данная строка не является палиндромом");
             }
            
 
             return View();
+        }
+
+        public string Index2(string textik = "test")
+        {
+            string itog;
+
+            ViewData["textik"] = textik;
+
+
+            char[] obrtextik = textik.ToCharArray();
+            Array.Reverse(obrtextik);
+            string finaltextik = new string(obrtextik);
+
+            if (textik == finaltextik)
+            {
+                return "Данная строка является палиндромом";
+            }
+            else
+            {
+                return "Данная строка не является палиндромом";
+            }
+
         }
     }
 }
